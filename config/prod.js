d768b12e2037a1ebcc6b4c33514fd421a54df880
Lokesh Karanam <sunnylokesh3@gmail.com>
2019-03-03 02:32:34 +0530

    added billing and client side

--- config/prod.js
+++ config/prod.js
@@ -1,10 +1,8 @@
-// prod.js - production keys here
-// should not be committed
-
 module.exports = {
   googleClientID: process.env.GOOGLE_CLIENT_ID,
-  googleClientSecret:process.env.GOOGLE_CLIENT_SECRET,
-  mongoURI:process.env.MONGO_URI,
-  cookieKey: process.env.COOKIE_KEY
+  googleClientSecret: process.env.GOOGLE_CLIENT_SECRET,
+  mongoURI: process.env.MONGO_URI,
+  cookieKey: process.env.COOKIE_KEY,
+  stripePublishableKey: process.env.STRIPE_PUBLISHABLE_KEY,
+  stripeSecretKey: process.env.STRIPE_SECRET_KEY
 };
-//mongodb+srv://prodbase:prodbase@emaily-prod-pvktq.mongodb.net/test?retryWrites=true
